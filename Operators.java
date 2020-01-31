ea783d52c6695c928f136b9f8572634b2f1febde
phlc <pedrocarvalho@outlook.com>
2020-01-31 09:56:59 -0300

    Operators.java added ?:

--- Operators.java
+++ Operators.java
@@ -10,5 +10,14 @@ public class Operators{
     System.out.println(++i);
 
     System.out.println(i);
+
+    boolean bool = true;
+    i = bool ? 11 : 44;
+    System.out.println(i);
+
+    bool = false;
+    i = bool ? 11 : 44;
+    System.out.println(i);
+
   }
 }
